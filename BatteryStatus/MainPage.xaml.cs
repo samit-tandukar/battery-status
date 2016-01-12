59b66baeaced1f9f1e48f084c6baa4b448661c7c
samit <samit_100@hotmail.com>
2016-01-12 11:32:09 +0545

    removed unnecessary usings and fixed naming convention

--- BatteryStatus/MainPage.xaml.cs
+++ BatteryStatus/MainPage.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -48,8 +37,8 @@ namespace BatteryStatus
             var getPercentage = (details.RemainingCapacityInMilliwattHours.Value / (double)details.FullChargeCapacityInMilliwattHours.Value);
             var status = details.Status;
             string per = getPercentage.ToString("##%");
-            progressInPer.Value = getPercentage * 100;
-            batteryStatus.Text = "Status: " + status + " - " + "Percentage: " + per;
+            ProgressInPer.Value = getPercentage * 100;
+            BatteryStatus.Text = "Status: " + status + " - " + "Percentage: " + per;
         }
     }
 }

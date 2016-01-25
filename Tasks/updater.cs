58886bae2236291aa5012506730ca9077bb04a33
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-01-25 16:41:02 +0100

    The background task retrieves current information about battery

--- Tasks/updater.cs
+++ Tasks/updater.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
 
 namespace Tasks
 {
-    class updater
+    public sealed class updater : IBackgroundTask
     {
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            BackgroundTaskDeferral _deferral = taskInstance.GetDeferral();
+            var details = Windows.Devices.Power.Battery.AggregateBattery.GetReport();
+            var getPercentage = (details.RemainingCapacityInMilliwattHours.Value / (double)details.FullChargeCapacityInMilliwattHours.Value);
+            var status = details.Status;
+            string per = getPercentage.ToString("##%");
+            double batteryPercentage = getPercentage * 100;
+            _deferral.Complete();
+        }
     }
 }
